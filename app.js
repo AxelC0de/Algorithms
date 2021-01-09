ae90efa01dc0722511e4d5a47a22b4fa681ea810
Redknight777999 <65612144+Redknight777999@users.noreply.github.com>
2021-01-09 09:20:29 +0300

    .

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Алгоритмы на JS
+console.log('App started...')
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -19,29 +20,29 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 //Бинарный поиск
 // сложность O(n), где n - длина списка
 
-function searchElement(arr, el) {
-	// left и right - позиция в массиве
-	let left = -1; //не может быть левее самого левого элемента
-	let right = arr.length; //не может быть правее самого правого элемента
+// function searchElement2(arr, el) {
+// 	// left и right - позиция в массиве
+// 	let left = -1; //не может быть левее самого левого элемента
+// 	let right = arr.length; //не может быть правее самого правого элемента
 
-	while (right - left > 1) { // если разница между ними больше единицы - то ищем дальше. Если меньше - то нашли
-		const mid = Math.floor((right - left) / 2); // вычисляем середину
+// 	while (right - left > 1) { // если разница между ними больше единицы - то ищем дальше. Если меньше - то нашли
+// 		const mid = Math.floor((right - left) / 2); // вычисляем середину
 
-		if (arr[mid] === el) {
-			return mid;
-		}
+// 		if (arr[mid] === el) {
+// 			return mid;
+// 		}
 
-		if (arr[mid] > el) {
-			right = mid;
-		} else {
-			left = mid;
-		}
-	}
+// 		if (arr[mid] > el) {
+// 			right = mid;
+// 		} else {
+// 			left = mid;
+// 		}
+// 	}
 
-	return -1
-}
+// 	return -1
+// }
 
-console.log(searchElement(arr, 8))
+// console.log(searchElement2(arr, 8))
 
 
-console.log('Hello')
+create database craig_list;
